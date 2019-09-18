4ab61fd5ea7b9777781a83886cac95fece86d6b5
BOLA <afaraitsulaimon@gmail.com>
2019-09-18 22:57:59 +1200

    added comment

--- logout.php
+++ logout.php
@@ -1,7 +1,13 @@
 <?php
+
+//START A SESSION
    session_start();
    require_once('config/db_config.php');
+   //DESTROY THE SESSION
    session_destroy();
+
+
+//REDIRECT TO LOGIN PAGE AFTER DESTROY
    header("location:index.php");
 
 ?>
